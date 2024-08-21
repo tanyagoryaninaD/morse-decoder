8eed1d12d2a4bf5ea363d6f57f15a60ff8f60afa
Татьяна Горянина <tanya200898@gmail.com>
2024-08-21 21:03:54 +0300

    init: done task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,57 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = expr.split('');
+    let letters = '';
+    let string = '';
+    for (let i=0; i<arr.length; i++) {
+        if (i%10==0 && i!=0) {
+            letters += ' '
+        }
+        letters += arr[i];
+    }
+    letters = letters.split(' ');
+
+    letters.forEach((item) => {
+        let subLetters = '';
+
+        if (item === '**********') {
+            item = ' ';
+        }
+
+        for (let i=0; i<item.length; i++) {
+            if (item[i] === ' ') {
+                subLetters += ' '
+                console.log(item.length, 'item')
+            }
+            if (i%2==0 && i!=0) {
+                subLetters += ' '
+            }
+            subLetters += item[i];
+        }
+        subLetters = subLetters.split(' ')
+
+        let word = subLetters.map(item => {
+            switch(item) {
+                case '00': 
+                    return item = ''
+                case '10':
+                    return item = '.'
+                case '11':
+                    return item = '-'
+                case ' ':
+                    return ' '
+            }
+        })
+        item = word.join('')
+        
+        if (item in MORSE_TABLE) {
+            string += MORSE_TABLE[item]
+        } else {
+            string += ' '
+        }
+    })
+    return string
 }
 
 module.exports = {
